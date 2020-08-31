3559d2c8daeb0914f334d2bbf116118331386e6d
VincentBouquet <bouquet.vincent704@orange.fr>
2020-08-31 16:59:05 +0200

    1rst

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import PlanetDescription from "./PlanetDescription";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [character, setCharacter] = useState(null)
+    const [planet, setPlanet] = useState(null)
+    const [characterId, setCharacterId] = useState("")
+
+    function handleChangeCharacterId(event) {
+        setCharacterId(event.target.value)
+    }
+
+    function loadCharacter() {
+        Axios.get("https://swapi.dev/api/people/"+characterId)
+            .then(function (response){
+                setCharacter(response.data)
+                loadHomeworld(response.data)
+            })
+    }
+
+    function loadHomeworld(characterData) {
+        Axios.get(characterData.homeworld)
+            .then(
+                ({data}) => setPlanet(data) // response => setPlanet(response.data)
+            )
+    }
+    return (
+        <div className="App">
+            <input type="number" value={characterId} onChange={handleChangeCharacterId}/>
+            <button onClick={loadCharacter}>Chercher</button>
+            {character?<h1>{character.name}</h1>:""}
+            {/*div>{JSON.stringify(planet)}</div>*/}
+            {/*planet?<div>{planet.name}</div>:""*/}
+            {planet?<PlanetDescription planetToDisplay={planet}/>:""}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/PlanetDescription.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function PlanetDescription({planetToDisplay}) {
+    return (<div>Planète d'origine: {planetToDisplay.name}</div>)
+}
+
+
+export default PlanetDescription;
